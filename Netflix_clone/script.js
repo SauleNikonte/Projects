0ab1f948f6f8769d426f55a4c0e8aa1c8498b7d3
Saule Nikonte <saule.nikonte@gmail.com>
2024-02-10 12:32:34 +0200

    Projects_from_BIT

--- Netflix_clone/script.js
+++ Netflix_clone/script.js
@@ -1,8 +1,10 @@
 // accordion//
-const accordion = document.querySelectorAll(".accordion");
+console.log("veikiu")
 
-for(let i = 0; i < accordion.length; i++) {
-    accordion[i].addEventListener("click", function() {
+const acc = document.querySelectorAll(".accordion");
+
+for(let i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
         this.classList.toggle("active")
 
         const desc = this.nextElementSibling
@@ -11,7 +13,30 @@ for(let i = 0; i < accordion.length; i++) {
             desc.style.maxHeight = null
         }
         else {
-            desc.style.maxHeight = desc.scrollHeight + "px";
+            desc.style.maxHeight = desc.scrollHeight + "px"
         }
 })
 };
+
+const signInButton = document.getElementById("signInButton");
+const signInModal = document.getElementById("modalSignIn");
+const closeButton = document.getElementById("closeButton");
+
+function showSignInModal() {
+    signInModal.showModal();
+    document.body.classList.add("dialog-opened");
+}
+signInButton.addEventListener("click", showSignInModal);
+
+function closeSignInModal() {
+    signInModal.close();
+    document.body.classList.remove('dialog-opened');
+}
+closeButton.addEventListener("click", closeSignInModal);
+
+// Close modal when "Escape" key is pressed
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'Escape') {
+        closeSignInModal();
+    }
+});
